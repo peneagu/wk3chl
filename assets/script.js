294710ccf8858a56f1bba7995ad1e81d2252d88d
peneagu <preciouseneagu8@icloud.com>
2022-07-22 11:48:22 -0700

    FINALLY!

--- assets/script.js
+++ assets/script.js
@@ -16,7 +16,7 @@ function writePassword() {
 
   //prompt//
   var length = prompt("Choose your password length! Min 8 Max 128");
-  if(length <8 || length > 128) {
+  if(length < 8 || length > 128) {
   alert("Select a minimum of 8 characters.");
   var length = prompt("Choose your password length! Min 8 Max 128");  
 }
@@ -25,48 +25,29 @@ var upper = confirm("Do you want to use UPPERCASE letters?");
 var specialChar = confirm ("Do you want to use $pec!@l Ch@r@acter$?");
 var numberic = confirm ("Do you want to use any numbers? 0-9");
 
-  // selected info
-  if (
-      upper !=true && lower !=true && specialChar !=true && numberic||
-      upper && lower !=true && specialChar !=true && numberic !=true||
-      upper !=true && lower && specialChar !=true && numberic !=true||
-      upper !=true && lower !=true && specialChar && numberic !=true
-      /*upper !=true && lower !=true && specialChar && numberic ||
-      upper !=true && lower && specialChar !=true && numberic ||
-      upper !=true && lower && specialChar && numberic !=true||
-      upper && lower !=true && specialChar !=true && numberic||
-      upper && lower !=true && specialChar && numberic !=true||
-      upper  && lower && specialChar !=true && numberic !=true*/)
-      { 
-      // verifying
-      alert("Select a minimum of 3 variables for your new password.")
-      var lower = confirm("Do you want to use lowercase letters?")
-      var upper = confirm("Do you want to use UPPERCASE letters?")
-      var specialChar = confirm ("Do you want to use $pec!@l Ch@r@acter$?")
-      var numberic = confirm ("Do you want to use any numbers? 0-9")
+  
 
-      if(upper && lower && specialChar && numberic){
+      if(upper && lower && specialChar && numberic) {
         content.options += content.upper + content.lower + content.specialChar + content.numberic;
       }
-      else if(!upper && lower && specialChar && numberic){
+      else if (!upper && lower && specialChar && numberic) {
         content.options += content.lower + content.specialChar + content.numberic;
       }
-      else if(upper && !lower && specialChar && numberic){
+      else if (upper && !lower && specialChar && numberic) {
         content.options += content.upper + content.specialChar + content.numberic;
       }
-      else if(upper && lower && !specialChar && numberic){
+      else if (upper && lower && !specialChar && numberic) {
         content.options += content.upper + content.lower + content.numberic;
       }
-      else if(upper && lower && specialChar && !numberic){
+      else if (upper && lower && specialChar && !numberic) {
         content.options += content.upper + content.lower + content.specialChar;
       }
 
       for (var i = 0; i<length; i++){
         random += content.options.charAt(Math.floor(Math.random()*content.options.length));
       }
-    }
       password.value = random
-  }
+}
 
 
 // event listener
