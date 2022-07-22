a10bac9878248dd3ff8b8ceee167b9d1e29659a7
peneagu <preciouseneagu8@icloud.com>
2022-07-21 17:10:29 -0700

    changesss

--- assets/script.js
+++ assets/script.js
@@ -8,37 +8,36 @@ var content = {
   upper: ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
   specialChar: ["!@#$%^&*"],
   numberic: ["1234567890"],
-  options = " ",
+  options: " ",
   }
 
 function writePassword() {
   var random = " ";
 
   //prompt//
-  var legnth = prompt("Choose your password length! Min 8 Max 128");
+  var length = prompt("Choose your password length! Min 8 Max 128");
   if(length <8 || length > 128) {
   alert("Select a minimum of 8 characters.");
   var length = prompt("Choose your password length! Min 8 Max 128");  
 }
+var lower = confirm("Do you want to use lowercase letters?");
+var upper = confirm("Do you want to use UPPERCASE letters?");
+var specialChar = confirm ("Do you want to use $pec!@l Ch@r@acter$?");
+var numberic = confirm ("Do you want to use any numbers? 0-9");
 
   // selected info
-  if (upper && lower && specialChar && numberic|| 
-      upper !=true && lower && specialChar && numberic||
-      upper && lower !=true && specialChar && numberic|| 
-      upper && lower && specialChar !=true && numberic |
-      upper && lower && specialChar && numberic != true||
-      upper !=true && lower !=true && specialChar !=true && numberic !=true||
+  if (
       upper !=true && lower !=true && specialChar !=true && numberic||
       upper && lower !=true && specialChar !=true && numberic !=true||
       upper !=true && lower && specialChar !=true && numberic !=true||
-      upper !=true && lower !=true && specialChar && numberic !=true||
-      upper !=true && lower !=true && specialChar && numberic ||
+      upper !=true && lower !=true && specialChar && numberic !=true
+      /*upper !=true && lower !=true && specialChar && numberic ||
       upper !=true && lower && specialChar !=true && numberic ||
       upper !=true && lower && specialChar && numberic !=true||
       upper && lower !=true && specialChar !=true && numberic||
       upper && lower !=true && specialChar && numberic !=true||
-      upper  && lower && specialChar !=true && numberic !=true||
-  )    
+      upper  && lower && specialChar !=true && numberic !=true*/)
+      { 
       // verifying
       alert("Select a minimum of 3 variables for your new password.")
       var lower = confirm("Do you want to use lowercase letters?")
@@ -47,27 +46,28 @@ function writePassword() {
       var numberic = confirm ("Do you want to use any numbers? 0-9")
 
       if(upper && lower && specialChar && numberic){
-        content.options +=content.upper + content.lower + content.specialChar + content.numberic;
+        content.options += content.upper + content.lower + content.specialChar + content.numberic;
       }
       else if(!upper && lower && specialChar && numberic){
-        content.options += + content.lower + content.specialChar + content.numberic;
+        content.options += content.lower + content.specialChar + content.numberic;
       }
       else if(upper && !lower && specialChar && numberic){
-        content.options +=content.upper + content.specialChar + content.numberic;
+        content.options += content.upper + content.specialChar + content.numberic;
       }
       else if(upper && lower && !specialChar && numberic){
-        content.options +=content.upper + content.lower + content.numberic;
+        content.options += content.upper + content.lower + content.numberic;
       }
       else if(upper && lower && specialChar && !numberic){
-        content.options +=content.upper + content.lower + content.specialChar;
+        content.options += content.upper + content.lower + content.specialChar;
       }
 
       for (var i = 0; i<length; i++){
         random += content.options.charAt(Math.floor(Math.random()*content.options.length));
       }
+    }
       password.value = random
+  }
 
-}
 
 // event listener
 generateBtn.addEventListener("click", writePassword);
